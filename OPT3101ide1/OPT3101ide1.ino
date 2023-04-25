3b95df7f4ffa367081e773c3e44d579c6a73b96a
Orpheus Johansson <123548945+DarkMantrid@users.noreply.github.com>
2023-04-25 15:18:37 +0200

    Add files via upload

---
+++ OPT3101ide1/OPT3101ide1.ino
@@ -0,0 +1,90 @@
+#include "header.h"
+
+void setup()
+{
+  Serial.begin(9600);
+  myservo.write(90); // sätter sensor till 90
+  Wire.begin();
+  myservo.attach(servoPin); //sätter igång servo
+  // Wait for the serial port to be opened before printing
+  // messages (only applies to boards with native USB).
+  while (!Serial) {}
+
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+
+  sensor.init();
+  if (sensor.getLastError())
+  {
+    Serial.print(F("Failed to initialize OPT3101: error "));
+    Serial.println(sensor.getLastError());
+    while (1) {}
+  }
+
+  sensor.setContinuousMode();
+  sensor.enableDataReadyOutput(1);
+  sensor.setFrameTiming(32);
+  sensor.setChannel(OPT3101ChannelAutoSwitch);
+  sensor.setBrightness(OPT3101Brightness::Adaptive);
+  attachInterrupt(digitalPinToInterrupt(dataReadyPin), setDataReadyFlag, RISING);
+  sensor.enableTimingGenerator();
+}
+
+void loop()
+{
+  leftsensor = distances[0]; // Döper om vänster sensor från distances[0] till leftsensor
+  middlesensor = distances[1]; // Döper om mitt sensor från distances[1] till middlesensor
+  rightsensor = distances[2]; // Döper om höger sensor från distances[2] till rightsensor
+  if (sensor.isSampleDone())
+  {
+    sensor.readOutputRegs();
+
+    distances[sensor.channelUsed] = sensor.distanceMillimeters;
+
+    if (sensor.channelUsed == 2)
+    {
+      
+      for (uint8_t i = 0; i < 3; i++)
+      {
+        if (leftsensor<Safe && leftsensor<rightsensor) // if för höger sväng
+        {
+          angle = map(leftsensor, Safe, NotSafe, Straight, TurnRight);
+          myservo.write(angle);
+        }
+        else if (rightsensor<Safe && rightsensor<leftsensor) // if för vänster sväng
+        {
+          angle = map(rightsensor, Safe, NotSafe, Straight, TurnLeft);
+          myservo.write(angle);
+        }
+        else if (middlesensor<NotSafe && rightsensor<leftsensor) // if för höger sväng men om väggen framför närmar sig
+        {
+          angle = map(rightsensor, Safe, Near, Straight, TurnRight);// map funktion för smidigare sväng
+          myservo.write(angle);
+        }
+        else if (middlesensor<NotSafe && rightsensor>leftsensor) // if för vänster sväng men om väggen framför närmar sig
+        {
+          angle = map(rightsensor, Safe, Near, Straight, TurnLeft);
+          myservo.write(angle);
+        }
+        else myservo.write(Straight); // bilen kör framåt
+        /*
+        Serial.print("\n");
+        Serial.print("left sensor= ");
+        Serial.print(leftsensor);
+        Serial.print("\n");
+        Serial.print("Middle Sensor= ");
+        Serial.print(middlesensor);
+        Serial.print("\n");
+        Serial.print("Right Sensor= ");
+        Serial.print(rightsensor);
+        Serial.print("\n");*/
+      } 
+      
+    }
+    sensor.nextChannel();
+    sensor.startSample();
+  }
+}
---
+++ OPT3101ide1/header.h
@@ -0,0 +1,39 @@
+// Include the servo library:
+#include <Servo.h> // inkluderar bibliotek för servo
+#include <OPT3101.h> // inkluderar bibliotek för sensor
+#include <Wire.h>
+#include <L298N.h>
+Servo myservo; // Create a new servo object:
+#define servoPin 5 // definerar servos pin
+#define enA 8
+#define in1 6
+#define in2 7
+
+#define remotePin 4 // Pin nummer för start modulens remote pin
+int remoteVal = 0; //värde för startmodul
+
+int In1 = 6; //motor low pin
+int In2 = 7; //motor high pin
+int SPEED = 255; // motor hastighet, högsta.
+
+const uint8_t dataReadyPin = 2; // Pin nummer för sensor läsning
+int angle = 90; // Döper svängningsvinkel till angle
+int leftsensor = 0;
+int middlesensor = 1;
+int rightsensor = 2;
+// Namn för olika sväng vinklar för lättare ändring av svängvinkel
+int Straight = 140; // Döper rakt vinkel
+int TurnAngle = 15; // Det här är hur mycket servon kan svänga för varje riktning.
+int TurnLeft = Straight - TurnAngle; // Rakt - svängvinkel gör att bilen svänger till vänster om TurnLeft anropas.
+int TurnRight = Straight + TurnAngle; // Rakt + svängvinkel gör att bilen svänger till höger om TurnRight anropas.
+// namn för avstånd mätning
+int Near = 200;
+int NotSafe = 450;
+int Safe = 600;
+OPT3101 sensor; // döper sensorn
+int16_t distances[3]; // aktiverar tre punkter på sensor och döper dem till distances
+volatile bool dataReady = false;
+void setDataReadyFlag()
+{
+dataReady = true;
+}
